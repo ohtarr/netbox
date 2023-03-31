4689c7ee8dbe1b9809f7e9141148d8c80e43cd63
ohtarr <ohtarr@gmail.com>
2023-03-31 10:01:35 -0500

    Update settings.py

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -411,8 +411,7 @@ USE_X_FORWARDED_HOST = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Static files (CSS, JavaScript, Images)
-#STATIC_ROOT = BASE_DIR + '/static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR + '/static'
 #STATIC_URL = f'/{BASE_PATH}static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'project-static', 'dist'),
